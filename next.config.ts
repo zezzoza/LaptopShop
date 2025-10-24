76e4e7564cf4291126c5449e3cc746cdabe0d395
zezzoza <ssymko@mail.ru>
2025-10-24 17:01:30 +0500

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: "export",
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: "/LaptopShop",
+    assetPrefix: "/LaptopShop/",
+}
 
-export default nextConfig;
+export default nextConfig
